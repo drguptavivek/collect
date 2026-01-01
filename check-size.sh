19a61c779c1895bce29a5385ade43faa36ffb1b7
Vivek Gupta <drguptavivek@yahoo.com>
2026-01-01 18:08:12 +0530

    deleted logs etc

--- check-size.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-if [ $(ls -l collect_app/build/outputs/apk/selfSignedRelease/*.apk | awk '{print $5}') -gt $1 ];then
-  echo "APK increased to $(ls -l collect_app/build/outputs/apk/selfSignedRelease/*.apk | awk '{print $5}') bytes!"
-  exit 1
-fi
